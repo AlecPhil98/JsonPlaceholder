08ce63413783bb30f19e80e7e7697066e1e3661c
Alec Philavong <alecphil@live.com>
2024-06-04 14:50:36 -0500

    finished 1st exercise

--- scripts/todo.js
+++ scripts/todo.js
@@ -1,6 +1,39 @@
 "use strict"
 
-window.onload = ()=>{
+window.onload = () => {
 
-    
+
+
+
+
+    let todoDropdown = document.querySelector("#todoId")
+    todoDropdown.addEventListener("click", getUserData)
+
 }
+
+
+async function getUserData() {
+    let userInput = document.querySelector("#todoIdInput")
+    let results = document.querySelector("#results")
+    console.log(userInput.value)
+
+    let newString = ("https://jsonplaceholder.typicode.com/todos/").concat(userInput.value)
+    console.log(newString)
+
+    try {
+        let reponse = await fetch(newString);
+        if (!reponse.ok) {
+            throw new Error("gosh darnit");
+        }
+        let data = await reponse.json();
+        console.log(data)
+        
+        results.innerHTML = JSON.stringify(data)
+
+        
+
+    } catch (error) {
+        console.log(error);
+    }
+}
+
