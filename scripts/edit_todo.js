caea88fe3663c7b0c6ff8f55ff3ca6428cbab6a5
Alec Philavong <alecphil@live.com>
2024-06-11 17:49:51 -0500

    update push

---
+++ scripts/edit_todo.js
@@ -0,0 +1,18 @@
+"use strict"
+
+window.onload = () => {
+
+
+    const getTodoEditForm = document.querySelector("#toDoEditForm");
+
+    getTodoEditForm.addEventListener("submit", poppulateUpdateForm)
+
+}
+
+const poppulateUpdateForm = async (event) => {
+
+    event.preventDefault();
+
+    let newEdit = await 
+
+ }
